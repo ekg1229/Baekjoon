2e5230756c44e12cf604b82809c2e80042886934
ekg1229 <ekg1229@naver.com>
2021-07-09 18:58:57 +0900

    20210709

---
+++ _1308.c
@@ -0,0 +1,11 @@
+/*
+file name : 1308.c
+author : ekg1229
+created : 2021/07/09
+*/
+
+#include <stdio.h>
+int main(){
+    
+    return 0;
+}
--- _3052.c
+++ _3052.c
@@ -10,16 +10,23 @@ int main(){
     int arr[10];
     for(int i = 0; i < 10; i++){
         scanf("%d", (arr + i));
+    }
+
+    for(int i = 0; i < 10; i++){
         arr[i] %= 42;
     }
 
     //배열에 서로 다른 값이 몇 개 있는지 출력.
-    for(int i = 0; i < 10; i++){
-        for(int j = i + 1; j < 10; j++){
-            if(arr[i] != arr_1[j]) cnt++;
+    if (arr[0] == arr[1] ==arr[2] ==arr[3]==arr[4]==arr[5]==arr[6]==arr[7]==arr[8]==arr[9]) cnt = 0;
+    else if(arr[0] != arr[1] !=arr[2] != arr[3] != arr[4] != arr[5] != arr[6] != arr[7] != arr[8] != arr[9]) cnt = 9;
+    else{
+        for (int i = 1; i < 10; i++){
+        if (arr[0] != arr[i]) cnt++;
+        }
+        for (int j = 1; j < 8; j++){
+            for (int i = j+1; i < 10; i++) if (arr[j] == arr[i]) cnt--;
         }
     }
-    printf("%d", cnt);
-
+    printf("%d", cnt + 1);
     return 0;
 }
